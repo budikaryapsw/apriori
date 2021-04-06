ec315204f1e7421e839bf0f0317d4c981d1842ab
egabudiman <egabudiman20@gmail.com>
2021-04-06 23:49:28 +0700

    up

--- fungsi.php
+++ fungsi.php
@@ -1,7 +1,7 @@
 <?php
 
 function can_access_menu($menu){
-    if($_SESSION['apriori_parfum_level']==2 ){
+    if($_SESSION['apriori_parfum_level']==2 & ($menu=='hasil_rule' || $menu=='view_rule'||$menu=='grafik_transaksi' ||$menu=='hasil')){
         return true;
     }
     if($_SESSION['apriori_parfum_level']==1){
